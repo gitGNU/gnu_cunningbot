d0607ecb8c067913c4776a3dff3d434d594e69cc
Aidan Gauland <aidalgol@no8wireless.co.nz>
2011-06-12 18:20:24 +1200

    No function takes an output port as an argument, instead using the global out.

--- bbot.scm
+++ bbot.scm
@@ -10,26 +10,26 @@
 (define nick "aidalbot")
 (define user "aidalgol")
 (define name "Aidan Gauland")
-(define server "irc.example.com")
+;;(define server "irc.example.com")
+(define server "chat.freenode.net")
 (define port 6667)
 (define channels '("#aidalbot"))
 
 (define (process-line line)
   "Process a line from the IRC server."
   (cond ((string-match "^PING" line)
-         (pong line out))
+         (pong line))
         ((string-match "^:(.*)!.*@.* PRIVMSG (.*) :(.*)" line) =>
          (lambda (match) (handle-privmsg
                           `((nick . ,(match:substring match 1))
                             (target . ,(match:substring match 2))
-                            (message . ,(match:substring match 3)))
-                          out)))))
+                            (message . ,(match:substring match 3))))))))
 
-(define (pong line out)
+(define (pong line)
   "Reply, on OUT, to a ping (which is LINE)."
   (display (string-append "PONG" (substring line 4)) out))
 
-(define (handle-ctcp msg target out)
+(define (handle-ctcp msg target)
   "Respond to a CTCP PRIVMSG sent by TARGET."
   (display (string-append "Responding to CTCP message: " (format #f "~s" msg) " sent by " target))
   (newline)
@@ -41,11 +41,11 @@
   (newline)
   (display (string-append "QUIT" line-end) out))
 
-(define (handle-command line out)
+(define (handle-command line)
   "Handle a command and its arguments on LINE."
   (eval-string (string-append "(cmd-" line ")")))
 
-(define (handle-privmsg msg-fields out)
+(define (handle-privmsg msg-fields)
   "Parse and respond to PRIVMSGs."
   (let* ((message (assoc-ref msg-fields 'message)))
     (cond
@@ -53,12 +53,12 @@
      ((string-match "\x01(.*)\x01" message) =>
       (lambda (match)
         (display "CTCP message.")
-        (handle-ctcp (match:substring match 1) (assoc-ref msg-fields 'nick) out)
+        (handle-ctcp (match:substring match 1) (assoc-ref msg-fields 'nick))
         (newline)))
      ;; Treat a message of the form "[NICK]: quit" as a command.
      ((string-match (string-append "^" nick ": (.*)") message) =>
       (lambda (match)
-        (handle-command (match:substring match 1) out))))
+        (handle-command (match:substring match 1)))))
     (begin
       (display (string-append
                 "Message received from "
