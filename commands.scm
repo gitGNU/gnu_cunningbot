4424604f304f002ed91762555c12393c77085874
Aidan Gauland <aidalgol@no8wireless.co.nz>
2012-11-28 16:19:36 +1300

    Added a missing period to the `flay' command.

--- commands.scm
+++ commands.scm
@@ -1,7 +1,7 @@
 (define (cmd-flay target args)
   (if (string-null? args)
       (send-privmsg "Who do you want me to flay?" target)
-      (send-action (string-append "flays " args) target)))
+      (send-action (string-append "flays " args ".") target)))
 
 (define (cmd-say-hello target args)
   (send-privmsg
