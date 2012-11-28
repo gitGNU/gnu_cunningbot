72222229b5a60a4985e185ddea4264ea8763d756
Aidan Gauland <aidalgol@no8wireless.co.nz>
2012-11-28 19:49:41 +1300

    `sender' is now passed to commands.

--- commands.scm
+++ commands.scm
@@ -1,8 +1,8 @@
-(define (cmd-flay target args)
+(define (cmd-flay sender target args)
   (if (string-null? args)
       (send-privmsg "Who do you want me to flay?" target)
       (send-action (string-append "flays " args ".") target)))
 
-(define (cmd-say-hello target args)
+(define (cmd-say-hello sender target args)
   (send-privmsg
    "Hello!" target))
